1d07b00061903a6bce3ad06bbaf38576b5b14961
Bucky <jacobowens75@gmail.com>
2022-10-02 16:08:18 -0500

    cmon

--- src/Questions.js
+++ src/Questions.js
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Questions = (props) => {
-  const [count, setCount] = useState(0);
-
   function setAnswer(choice) {
     if (choice === props.userChoice) {
       return "selected";
